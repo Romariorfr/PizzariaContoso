ef36a70e844219a167d2a25a64118f4de14ef75b
Romario F <romarioferreirarezende@hotmail.com>
2024-02-28 19:29:06 -0300

    atualizado banco de dados

---
+++ Migrations/20240228222401_ModelRevision.cs
@@ -0,0 +1,142 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ContosoPizza.Migrations
+{
+    /// <inheritdoc />
+    public partial class ModelRevision : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Topping_Pizzas_PizzaId",
+                table: "Topping");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Topping_PizzaId",
+                table: "Topping");
+
+            migrationBuilder.DropColumn(
+                name: "PizzaId",
+                table: "Topping");
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "Calories",
+                table: "Topping",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: 0m);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Sauce",
+                type: "TEXT",
+                maxLength: 100,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsVegan",
+                table: "Sauce",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Pizzas",
+                type: "TEXT",
+                maxLength: 100,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldNullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "PizzaTopping",
+                columns: table => new
+                {
+                    PizzasId = table.Column<int>(type: "INTEGER", nullable: false),
+                    ToppingsId = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PizzaTopping", x => new { x.PizzasId, x.ToppingsId });
+                    table.ForeignKey(
+                        name: "FK_PizzaTopping_Pizzas_PizzasId",
+                        column: x => x.PizzasId,
+                        principalTable: "Pizzas",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_PizzaTopping_Topping_ToppingsId",
+                        column: x => x.ToppingsId,
+                        principalTable: "Topping",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PizzaTopping_ToppingsId",
+                table: "PizzaTopping",
+                column: "ToppingsId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PizzaTopping");
+
+            migrationBuilder.DropColumn(
+                name: "Calories",
+                table: "Topping");
+
+            migrationBuilder.DropColumn(
+                name: "IsVegan",
+                table: "Sauce");
+
+            migrationBuilder.AddColumn<int>(
+                name: "PizzaId",
+                table: "Topping",
+                type: "INTEGER",
+                nullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Sauce",
+                type: "TEXT",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldMaxLength: 100);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Pizzas",
+                type: "TEXT",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldMaxLength: 100);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Topping_PizzaId",
+                table: "Topping",
+                column: "PizzaId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Topping_Pizzas_PizzaId",
+                table: "Topping",
+                column: "PizzaId",
+                principalTable: "Pizzas",
+                principalColumn: "Id");
+        }
+    }
+}
--- Models/Topping.cs
+++ Models/Topping.cs
@@ -1,7 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace ContosoPizza.Models;
 
 public class Topping
 {
+    [Required]
+    [MaxLength(100)]
     public int Id { get; set; }
     public string? Name { get; set; }
+
+    public decimal Calories { get; set; }
+
+    public ICollection<Pizza>? Pizzas { get; set; }
 }
