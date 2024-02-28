9323efea67968bee3ab73e11ed82c86d765be916
Romario F <romarioferreirarezende@hotmail.com>
2024-02-27 21:05:50 -0300

    instalado dependencias, criado PizzaContext

---
+++ Data/PizzaContext.cs
@@ -0,0 +1,16 @@
+using ContosoPizza.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ContosoPizza.Data
+{
+    public class PizzaContext : DbContext
+    {
+        public PizzaContext(DbContextOptions<PizzaContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Pizza> Pizzas { get; set; }
+
+    }
+}
